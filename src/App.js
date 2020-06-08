2aff4cf2b53b7e25c287d0963b7b31de9e3fb641
smmerrit <smerritt98@gmail.com>
2020-06-08 15:35:15 -0700

    preview components and scss added to store

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx'
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+
+
+
+
+
 
 function App() {
   return (
     <div>
   <switch>
 <Route exact path ="/" component={HomePage} />
-<Route path='/hats' component={HatsPage} />
+<Route path='/shop' component={ShopPage} />
       </switch>
     </div>
   );
