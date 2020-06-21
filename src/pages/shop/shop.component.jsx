dcb564ba01ecb96cca3bc8e614f37a505939d0ec
smmerrit <smerritt98@gmail.com>
2020-06-21 12:19:58 -0700

    Redux conversions for collection, directory and collection overview and session storage added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/preview-collection/collection-preview.component'
-//import 'collection-preview.styles.scss';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.components'
+const ShopPage = ({ collections }) => 
+(<div className='shop-page'>
+        <CollectionsOverview/>
+        </div> 
+        
+)
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props)
 
-        this.state={
-            collections: SHOP_DATA
-
-        };
-    }
-
-    render() {
-        const {collections} = this.state
-        return (<div className='shop-page'>
-{
-    collections.map(({id, ...otherCollectionProps}) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-    ))
-}
-        </div> )
-
-    }
-}
 export default ShopPage;
