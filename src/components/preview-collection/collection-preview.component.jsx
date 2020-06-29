b4e4a777743e2209c413df0be0ba44d80676e1ad
smmerrit <smerritt98@gmail.com>
2020-06-29 12:14:05 -0700

    Shop page fixed

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component.jsx'
 import './collection-preview.styles.scss'
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1> 
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
@@ -13,6 +13,6 @@ const CollectionPreview = ({title, items}) => (
                 ))}
         </div>
     </div>
+);
 
-)
 export default CollectionPreview;
